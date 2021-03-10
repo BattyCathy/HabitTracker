d8f18f67332bcbe877aad7cda4d5c2a6e89ba923
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-09 18:18:18 -0800

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Austin Roach on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
